103a21e1a239f9087f692ca7891dd7800f8d4601
dave <dave@DESKTOP-ONGTTRI>
2019-08-01 18:15:06 +0100

    Commit 2 - midpoint

---
+++ 04.Booleans/Entrypoint.cs
@@ -0,0 +1,12 @@
+﻿class Entrypoint
+{
+    static void Main()
+    {
+        bool isTheBiscuitTinFull = false;
+        bool isTheBiscuitTinEmpty = true;
+        bool isTheFirstNumberBigger = 4 > 6;
+        System.Console.WriteLine(isTheFirstNumberBigger);
+        //True or false statements only, can be used for comparison
+        //if you wish to change a variable Ctrl+R+R
+    }
+}
---
+++ 05.Characters/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        //changes encoding for the console, only if special characters are required
+        char theCharacterX = 'x';
+        System.Console.WriteLine(theCharacterX);
+        char unicodeCharacter = '\u02e6';
+        System.Console.WriteLine(unicodeCharacter);
+        //When writing in ascii or unicode, \u0000 , change the zeros with the ascii code where required
+        char unicodeTest = '\u1760';
+        Console.WriteLine(unicodeTest);
+
+    }
+}
