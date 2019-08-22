800a211fe9ca5115f21e4dd1f6ed958bfae4a31a
dave <dave@DESKTOP-ONGTTRI>
2019-08-22 19:01:59 +0100

    Mid Point commit, back at it again

---
+++ 09.Stringpt2/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        int bigNumber = 3456741;
+
+        System.Console.WriteLine(bigNumber.ToString().Contains("5"));
+        System.Console.WriteLine(bigNumber.ToString().IndexOf("5"));
+        System.Console.WriteLine(bigNumber.ToString().LastIndexOf("5"));
+        //can't use contains index of etc on Ints, needs to be converted to string first
+
+        string iLearn = "I am learning ";
+        string nameOfCourse = "C# programming course";
+
+        string concantenated = iLearn + nameOfCourse;
+        string concantenated2 = iLearn + nameOfCourse + bigNumber;
+
+        System.Console.WriteLine(iLearn + nameOfCourse);
+        System.Console.WriteLine(concantenated);
+        System.Console.WriteLine(concantenated2);
+
+
+    }
+}
---
+++ 10. StringBuilding/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        string nameOfCharacter = "Link ";
+        int damageTaken = 100;
+        string spellName = "Fireball";
+
+        string damageReport = "The " + nameOfCharacter + "character took " + damageTaken + " damage from " + spellName;
+
+        System.Console.WriteLine(damageReport);
+        // works but over complicated
+
+        string damageReportV2 = string.Format("The {0}character took {1} damage from {2} ", nameOfCharacter, damageTaken, spellName);
+        System.Console.WriteLine(damageReportV2);
+        //Curly Bois are used as the strings now act like an array, and they are getting the index number from it
+        string damageReportV21 = string.Format("The {0}character took {1} damage from {2} , and the character {0}is now dead", nameOfCharacter, damageTaken, spellName);
+        System.Console.WriteLine(damageReportV21);
+        //You can retarget indexs at any point
+        string damageReportV3 = $"The {nameOfCharacter}character took {damageTaken} damage from {spellName} ";
+        System.Console.WriteLine(damageReportV3);
+        //interpolation is much easier to read and write
+    }
+}
